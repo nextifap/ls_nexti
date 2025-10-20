ce3bd9d303d56457a533928f6ef72bf9be62efa7
IanCaleb <ivusaw1227@gmail.com>
2025-10-20 20:58:03 -0300

    Implementa a classe material

---
+++ backend/app/Models/Material.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Material extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'writer_id',
+        'category_id',
+        'title',
+        'autor',
+        'doi',
+        'description',
+        'publication_data',
+        'cover',
+        'doc',
+        'page_number',
+        'type',
+        'status',
+    ];
+    
+    /*
+    // Relação com Writer
+    public function writer()
+    {
+        return $this->belongsTo(Writer::class);
+    }
+
+    */
+
+    /*
+    // Relação com Category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    */
+}
