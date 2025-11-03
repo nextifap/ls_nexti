3d6f9832d41fb6d55596819b402ea94e276d83f4
IanCaleb <ivusaw1227@gmail.com>
2025-11-03 14:29:32 -0300

    Ajusta os elementos da home como componentes e troca a estilização de css para taillwind

--- frontend/front/src/components/recomendacao/index.jsx
+++ frontend/front/src/components/recomendacao/index.jsx
@@ -1,18 +1,46 @@
-import './style.css'
-
 export function Recomendacao ({nome, curso, texto, foto}) {
     return(
-        <div className='container-recomendacao'>
-            <div className='header-recomendacao'>
-                    <img className='foto' src={foto} alt="" />
-                    <div>
-                        <p className='nome'>{nome}</p>
-                        <p className='curso'>{curso}</p>
+
+        <div>
+            <div className='header-recomendacao flex mb-5'>
+                    <img className='rounded-full w-[4rem] h-[4rem]' src={foto} alt="foto-perfil" />
+
+                    <div className='flex flex-col justify-center ml-5'>
+                        <p className='font-medium text-lg'>{nome}</p>
+                        <p className='text-left text-base'>{curso}</p>
                     </div>
             </div>
 
-            <p className='texto'>{texto}</p>
-        </div>
+            <p className='text-[#2A2A2A] text-left text-lg'>{texto}</p>
+        </div> 
         
     )
 }
+
+/*
+
+.header-recomendacao .foto{
+    width: 5vw;
+    height: 5vw;
+    border-radius: 50%;
+    border-color: #EB973E;
+    border-width: 2%;
+    border-style: solid;
+}
+
+.container-recomendacao .texto{
+    padding-top: 0.90vw;
+    color: #2A2A2A  ;
+    font-size: 1.1vw;
+    font-weight: 500;
+
+}
+
+.container-recomendacao .nome {
+    font-weight: 500;
+    font-size: 1.40vw;
+    margin: 0;
+    padding-bottom: 1vw;
+    padding-top: 1vw;
+}
+*/
