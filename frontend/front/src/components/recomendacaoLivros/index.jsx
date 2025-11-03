732a3a50368cc11a505e841c643370e1500a7a25
IanCaleb <ivusaw1227@gmail.com>
2025-11-03 14:29:48 -0300

    Ajusta os elementos da home como componentes e troca a estilização de css para taillwind

---
+++ frontend/front/src/components/recomendacaoLivros/index.jsx
@@ -0,0 +1,95 @@
+import { Link } from "react-router-dom"
+import { Livro } from "../livro"
+
+export function RecomendacaoLivros () {
+    return(
+        <section className="flex flex-col items-center p-16">
+            <h3 className="text-3xl flex flex-col text-center">VISITE NOSSA 
+                <span className="text-[#213E60]">
+                    ESTANTE VIRTUAL
+                </span>
+            </h3>
+
+            <p className="md:w-[50%] w-full mb-10 mt-6 text-center">Descubra uma seleção especial de livros e materiais gratuitos para impulsionar seus estudos em tecnologia e programação. Explore conteúdos feitos por especialistas e desenvolva seu aprendizado no seu ritmo.</p>
+
+            <div className="flex justify-between md:flex-row flex-col items-center">
+                <Livro className="pequeno" livroId={1}/>
+                <Livro className="pequeno" livroId={2}/>
+                <Livro className="pequeno" livroId={3}/>
+            </div>
+
+            <Link to={"/estante-virtual"}>
+                    <div className="relative overflow-hidden group w-fit">
+                        <p className="relative bg-[#FFB363] text-[#2E2E2E] text-lg md:w-[25vw] w-full flex justify-center items-center p-5 rounded-[5rem] mt-5 border-[0.2rem] border-[#EB973E] hover:border-[#996228] font-medium overflow-hidden duration-300">
+                            <span className="relative z-10 transition-colors duration-1000 group-hover:text-white">
+                                Outros titulos
+                            </span>
+
+                            {/* Quadrado laranja com transição da esquerda para a direita */}
+                            <span className="absolute top-0 left-[-100%] w-full h-full bg-[#fa9d3c] rounded-[5rem] z-0 transition-all duration-1000 group-hover:left-0"></span>
+                        </p>
+                    </div>
+            </Link>
+        </section>
+    )
+}
+
+/*
+
+
+.homeContent .container-livros{
+    display: flex;
+    flex-direction: column;
+    align-items: center
+}
+
+.homeContent .container-livros p{
+    text-align: center;
+}
+
+.homeContent .container-livros .livros{
+    display: flex;
+    justify-content: space-between;
+    width: 90vw;
+}
+
+.homeContent .container-livros button{
+    margin-top: 6vw;
+    margin-bottom: 6vw;
+    font-size: 1.21vw;
+    width: 30vw;
+    height: 4vw;
+    border-radius: 1.16vw;
+    border-width: 2px;
+    border-style: solid;
+    background: #FFB363;
+    border-color: #EB973E;
+    color: #2E2E2E;
+    font-weight: 600;
+    cursor: pointer;
+}
+
+.homeContent .visite-title {
+    font-size: 3vw;
+    padding-top: 5vw;
+    padding-bottom: 1.50vw;
+    margin: 0;
+}
+
+.homeContent .visite-paragraph{
+    font-size: 1.25vw;
+    padding-bottom: 1.35vw;
+    margin: 0;
+    margin-bottom: 2em;
+}
+
+.homeContent .container-livros .visite-paragraph {
+    max-width: 50vw;
+}
+
+.homeContent .container-livros button:hover {
+    background-color: #d4833e;
+}
+
+*/
+
--- frontend/front/src/pages/Home.jsx
+++ frontend/front/src/pages/Home.jsx
@@ -2,62 +2,19 @@ import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { Livro } from "../components/livro";
 import { Navigation } from "../components/navigation";
-import { Recomendacao } from "../components/recomendacao";
+import { RecomendacaoList } from "../components/recomendacaoList";
+import { Banner } from "../components/banner";
+import { Editorial } from "../components/editorial";
+import { RecomendacaoLivros } from "../components/recomendacaoLivros";
 
 import '../styles/Home.css'
 import { Link } from "react-router-dom";
 
-import ImgCorpoEditorial from '../assets/img/img-editorial.svg'
-
 import ScrollToTop from "../features/ScrollTop";
 
-import ImgPerfil1 from '../assets/img/fotosPerfil/fotoPerfil1.jpeg'
-import ImgPerfil2 from '../assets/img/fotosPerfil/fotoPerfil2.jpeg'
-import ImgPerfil3 from '../assets/img/fotosPerfil/fotoPerfil3.jpeg'
-import ImgPerfil4 from '../assets/img/fotosPerfil/fotoPerfil4.jpeg'
-import ImgPerfil5 from '../assets/img/fotosPerfil/fotoPerfil5.jpeg'
-import ImgPerfil6 from '../assets/img/fotosPerfil/fotoPerfil6.jpeg'
 
 function Home () {
 
-    const recomendacoes = [
-        {
-          nome: "Leonardo",
-          curso: "Analista de Sistemas",
-          texto: "Me ajudou a desenvolver habilidades essenciais para o mercado. Recomendo para todos que querem crescer na área!",
-          foto: ImgPerfil1
-        },
-        {
-          nome: "Lucas Sousa",
-          curso: "Cientista de dados",
-          texto: "Sempre admirei a dedicação da equipe. O aprendizado foi enriquecedor e a experiência prática fez toda a diferença.",
-          foto: ImgPerfil2
-        },
-        {
-          nome: "Icaro Moreira",
-          curso: "Cientista da Computação",
-          texto: "É ótimo ver uma abordagem tão humana e acessível à educação digital. Recomendo para meus alunos!",
-          foto: ImgPerfil3
-        },
-        {
-          nome: "Gabriel Lucas",
-          curso: "Desenvolvimento Web",
-          texto: "A didática utilizada foi incrível! Aprendi muito sobre frameworks modernos e como aplicá-los em projetos reais.",
-          foto: ImgPerfil4
-        },
-        {
-          nome: "Ian Caleb",
-          curso: "Desenvolvedor Web",
-          texto: "Um curso bem estruturado, com material de qualidade e professores que realmente entendem do assunto.",
-          foto: ImgPerfil5
-        },
-        {
-          nome: "Pedro Alexandre",
-          curso: "Designer Gráfico",
-          texto: "Aprendi muito sobre cibersegurança e melhores práticas para proteger sistemas contra ataques.",
-          foto: ImgPerfil6
-        }
-      ];
       
     return (
         <div>
@@ -69,56 +26,16 @@ function Home () {
             </div>
             <Header />
             <Navigation />
+            
             <div className="homeContent">
-              <section className="Banner">
-                <div className="textos">
-                  <h1 className="banner-title">Publique seu <br />livro conosco</h1>
-                  <p className="banner-paragraph">Faça parte de uma comunidade apaixonada pela palavra escrita. Nossa equipe editorial está pronta para elevar seu trabalho a novos horizontes.</p>
-                  <Link to={"/nossos-servicos"}><button className="banner-button">Saiba Mais</button></Link>
-                </div>   
-              </section>
 
-              <section className="container-livros">
-                  <h3 className="visite-title">VISITE NOSSA <span style={{ color: "#213E60" }}>ESTANTE VIRTUAL</span></h3>
-                  <p className="visite-paragraph">Descubra uma seleção especial de livros e materiais gratuitos para impulsionar seus estudos em tecnologia e programação. Explore conteúdos feitos por especialistas e desenvolva seu aprendizado no seu ritmo.</p>
-                  <div className="livros">
-                      <Livro className="pequeno" livroId={1}/>
-                      <Livro className="pequeno" livroId={2}/>
-                      <Livro className="pequeno" livroId={3}/>
-                  </div>
+              <Banner/>
 
-                  <Link to={"/estante-virtual"}><button>Outros títulos</button></Link>
-              </section>
+              <RecomendacaoLivros/>
 
-              <section className="container-recomendacoes">
-                  <div className="header-recomendacoes">
-                      <h3 className="recomendacoes-title">QUEM CONHECE <span style={{ color: "#213E60" }}>RECOMENDA</span></h3>
-                      <p className="recomendacoes-paragraph">Quem passa pela Learn Skills vive uma jornada de aprendizado transformadora. Nossos alunos e parceiros reconhecem o cuidado, a dedicação e a qualidade de tudo o que oferecemos.</p>
-                  </div>
+              <RecomendacaoList/>
 
-                  <div className="cards-recomendacoes"> 
-                          {recomendacoes.map((rec, index) => (
-                          <Recomendacao
-                              key={index}
-                              nome={rec.nome}
-                              curso={rec.curso}
-                              texto={rec.texto}
-                              foto={rec.foto}
-                          />
-                          
-                          ))}
-                      </div>  
-              </section>
-
-              <section className="container-editorial-section">
-                  <div>
-                      <h3>COMO PUBLICAR <br /><span style={{ color: "#213E60" }}>SEU LIVRO</span></h3>
-                      <p>Conheça a equipe por trás das palavras! Nosso corpo editorial é a engrenagem que impulsiona a qualidade e a precisão em cada texto.</p>
-                      <Link to={"/submeter-material"}><button>Saiba mais</button></Link>
-                  </div>
-
-                  <img src={ImgCorpoEditorial} alt="" />
-              </section>
+              <Editorial/>
 
             </div>
             
