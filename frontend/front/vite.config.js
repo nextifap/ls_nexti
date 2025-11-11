005a02c07553341af744426ce756d30ca3b713f9
IanCaleb <ivusaw1227@gmail.com>
2025-11-11 10:10:44 -0300

    configuração servidor local

--- frontend/front/vite.config.js
+++ frontend/front/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+/*
 export default defineConfig({
   plugins: [react()],
 
@@ -9,6 +10,22 @@ export default defineConfig({
     include: ['sweetalert2']
   }
 })
+*/
+
+
+export default defineConfig({
+  plugins: [react()],
+
+  optimizeDeps: {
+    include: ['sweetalert2']
+  },
+
+  server: {
+    host: true, // ou '0.0.0.0'
+    port: 8080, // opcional
+  },
+})
+
 
 
 
